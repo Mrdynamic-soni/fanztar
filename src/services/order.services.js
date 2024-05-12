d7d5403e8b3b7cb5abe49b0223598e475d1c9055
Mrdynamic-soni <ssoni5930@gmail.com>
2024-05-12 19:15:27 +0530

    handled case for  valid order

--- src/services/order.services.js
+++ src/services/order.services.js
@@ -1,25 +1,58 @@
 import { mobileData } from "../utils/mobileData.js";
 
-export const mobileDataListPrice = (codeList) => {
-  let price = 0;
-  const parts = [];
+const isValidOrder = (partsArray) => {
+  const requiredPartTypes = [
+    "screen",
+    "camera",
+    "chargingPortType",
+    "operatingSystem",
+    "bodyType",
+  ];
+  const partTypeCounts = {};
 
-  const partsMap = new Map();
-  mobileData.parts.forEach((part) =>
-    partsMap.set(part.code.toUpperCase(), part)
-  );
-
-  for (const code of codeList) {
-    const part = partsMap.get(code.toUpperCase());
-    if (part) {
-      price += part.price;
-      parts.push(part.part);
+  for (let partCode of partsArray) {
+    for (let partType of requiredPartTypes) {
+      if (mobileData.parts[partType].some((part) => part.code === partCode)) {
+        partTypeCounts[partType] = (partTypeCounts[partType] || 0) + 1;
+        break;
+      }
     }
   }
 
+  for (let partType of requiredPartTypes) {
+    if (!partTypeCounts[partType] || partTypeCounts[partType] > 1) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+export const mobileDataListPrice = (partsArray) => {
+  if (!isValidOrder(partsArray)) {
+    return "Invalid order";
+  }
+
+  let totalPrice = 0;
+  let selectedParts = [];
+
+  for (let partCode of partsArray) {
+    for (let partType in mobileData.parts) {
+      const part = mobileData.parts[partType].find(
+        (part) => part.code === partCode
+      );
+      if (part) {
+        totalPrice += part.price;
+        selectedParts.push(part?.part);
+        break;
+      }
+    }
+  }
   return {
-    order_id: Math.floor(Math.random() * 100) + 1,
-    price: price.toFixed(2),
-    parts: parts,
+    order_id: generateOrderId(),
+    price: totalPrice.toFixed(2),
+    parts: selectedParts,
   };
 };
+
+const generateOrderId = () => Math.floor(Math.random() * 100) + 1;
